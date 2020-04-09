69a8ea771e71bc458ed0b901138e8307f66d3669
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-04-09 16:21:14 +0300

    Format code in position.js

--- controllers/position.js
+++ controllers/position.js
@@ -1,55 +1,53 @@
-const Position = require('../models/Position')
-const errorHandler = require('../utils/errorHandler')
-
-
+const Position = require("../models/Position");
+const errorHandler = require("../utils/errorHandler");
 
 module.exports.getByCategoryId = async function(request, response) {
-    try {
-        const positions = await Position.find({
-            category: request.params.categoryId,
-            user: request.user.id
-        })
-        response.status(200).json(positions)
-    } catch (error) {
-        errorHandler(response, error)
-    }
-}
+  try {
+    const positions = await Position.find({
+      category: request.params.categoryId,
+      user: request.user.id
+    });
+    response.status(200).json(positions);
+  } catch (error) {
+    errorHandler(response, error);
+  }
+};
 
 module.exports.createPosition = async function(request, response) {
-    try {
-        const position = new Position({
-            name: request.body.name,
-            cost: request.body.cost,
-            category: request.body.category,
-            name: request.user.id
-        })
-        await position.save()
-        response.status(201).json(position)
-    } catch (error) {
-        errorHandler(response, error)
-    }
-}
+  try {
+    const position = new Position({
+      name: request.body.name,
+      cost: request.body.cost,
+      category: request.body.category,
+      name: request.user.id
+    });
+    await position.save();
+    response.status(201).json(position);
+  } catch (error) {
+    errorHandler(response, error);
+  }
+};
 
 module.exports.deleteByID = async function(request, response) {
-    try {
-        await Position.remove({_id: request.params.id})
-        response.status(200).json({
-            message: "Position is deleted"
-        })
-    } catch (error) {
-        errorHandler(response, error)
-    }
-}
+  try {
+    await Position.remove({ _id: request.params.id });
+    response.status(200).json({
+      message: "Position is deleted"
+    });
+  } catch (error) {
+    errorHandler(response, error);
+  }
+};
 
 module.exports.updateByID = async function(request, response) {
-    try {
-        const position = await Position.findOneAndUpdate(
-            {_id: request.params.id},
-            {$set: request.body},
-            {new: true}
-             )
-        response.status(200).json(position)
-    } catch (error) {
-        errorHandler(response, error)
-    }
-}
+  try {
+    const position = await Position.findOneAndUpdate(
+      { _id: request.params.id },
+      { $set: request.body },
+      { new: true }
+    );
+    response.status(200).json(position);
+  } catch (error) {
+    errorHandler(response, error);
+  }
+};

b70f6c068d5144505644b49b666d51488beb1a7c
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-04-09 16:29:52 +0300

    Add method get all by id for category

--- controllers/category.js
+++ controllers/category.js
@@ -1,43 +1,41 @@
-const Category = require('../models/Category')
-const errorHandler = require('../utils/errorHandler')
+const Category = require("../models/Category");
+const errorHandler = require("../utils/errorHandler");
 
 module.exports.getAll = async function(request, response) {
-    try {
-      const categories = await Category.find({user: request.user.id})
-      response.status(200).json(categories)          
-    } catch (error) {
-      errorHandler(error)  
-    }
-}
+  try {
+    const categories = await Category.find({ user: request.user.id });
+    response.status(200).json(categories);
+  } catch (error) {
+    errorHandler(error);
+  }
+};
 
 module.exports.getByID = async function(request, response) {
-    try {
-        
-    } catch (error) {
-      errorHandler(error)  
-    }
-}
+  try {
+      const category = await Category.findById(request.params.id)
+      response.status(200).json(category)
+  } catch (error) {
+    errorHandler(error);
+  }
+};
 
 module.exports.deleteByID = async function(request, response) {
-    try {
-        
-    } catch (error) {
-      errorHandler(error)  
-    }
-}
+  try {
+  } catch (error) {
+    errorHandler(error);
+  }
+};
 
 module.exports.createCategory = async function(request, response) {
-    try {
-        
-    } catch (error) {
-      errorHandler(error)  
-    }
-}
+  try {
+  } catch (error) {
+    errorHandler(error);
+  }
+};
 
 module.exports.updateCategory = async function(request, response) {
-    try {
-        
-    } catch (error) {
-      errorHandler(error)  
-    }
-}
+  try {
+  } catch (error) {
+    errorHandler(error);
+  }
+};

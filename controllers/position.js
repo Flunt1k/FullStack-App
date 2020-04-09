9866f8ffc349fd377fbc960aea36b9c369359001
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-04-09 16:08:17 +0300

    Add getting category by ID from server

--- controllers/position.js
+++ controllers/position.js
@@ -1,15 +1,40 @@
-module.exports.getByCategoryId = function(request, response) {
-    
+const Position = require('../models/Position')
+const errorHandler = require('../utils/errorHandler')
+
+
+
+module.exports.getByCategoryId = async function(request, response) {
+    try {
+        const positions = await Position.find({
+            category: request.params.categoryId,
+            user: request.user.id
+        })
+        response.status(200).json(positions)
+    } catch (error) {
+        errorHandler(response, error)
+    }
 }
 
-module.exports.createPosition = function(request, response) {
-    
+module.exports.createPosition = async function(request, response) {
+    try {
+        
+    } catch (error) {
+        errorHandler(response, error)
+    }
 }
 
-module.exports.deleteByID = function(request, response) {
-    
+module.exports.deleteByID = async function(request, response) {
+    try {
+        
+    } catch (error) {
+        errorHandler(response, error)
+    }
 }
 
-module.exports.updateByID = function(request, response) {
-    
+module.exports.updateByID = async function(request, response) {
+    try {
+        
+    } catch (error) {
+        errorHandler(response, error)
+    }
 }

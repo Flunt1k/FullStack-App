52bfc2a1eac9a0b50e16597188c5d46fbfa3fee0
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-03 12:17:57 +0300

    Add method for getting all orders

--- controllers/order.js
+++ controllers/order.js
@@ -1,8 +1,43 @@
 const Order = require('../models/Order')
 const errorHandler = require('../utils/errorHandler')
 
-module.exports.getAll = function(request, response) {
-    
+module.exports.getAll = async function(request, response) {
+    const query = {
+        user: request.user.id,
+    }
+
+    //start date
+    if (request.query.start) {
+        query.date = {
+            //greater or equal
+            $gte: request.query.start
+        }
+    }
+
+    if (request.query.end) {
+        if (!query.date) {
+            query.date = {}
+        }
+
+        //less or equal
+        query.date['$lte'] = request.query.end
+    }
+
+    if (request.query.order){
+        query.order = +request.query.order
+    }
+
+    try {
+        const orders = await Order
+            .find(query)
+            .sort({date: -1})
+            .skip(+request.query.offset)
+            .limit(+request.query.limit)
+
+            response.status(200).json(orders)
+    } catch (e) {
+        errorHandler(response, e)
+    }
 }
 
 module.exports.createOrder = async function(request, response) {
@@ -19,7 +54,7 @@ module.exports.createOrder = async function(request, response) {
         }).save()
    
         response.status(201).json(order)
-    } catch (error) {
+    } catch (e) {
         errorHandler(response, error)
     }   
 }

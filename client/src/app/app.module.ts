1de0af350db834aefba5a5cd407786e90b8a249f
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-04 23:03:27 +0300

    Split layout for login and auth pages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginPageComponent, AuthLayoutComponent, SiteLayoutComponent],
+  declarations: [AppComponent, LoginPageComponent, AuthLayoutComponent, SiteLayoutComponent, RegisterPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],

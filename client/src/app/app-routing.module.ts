fcb3cb1723b669d1e68db30a0a077b2ab83141a8
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-04 22:51:33 +0300

    Add layouts and login page to routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, Router } from '@angular/router'
+import { RouterModule, Routes, Router } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 
 const routes: Routes = [
-    {path: 'login', component: LoginPageComponent}
-]
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [{ path: 'login', component: LoginPageComponent }],
+  },
+  {
+    path: '',
+    component: SiteLayoutComponent,
+    children: [],
+  },
+];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes)
-    ],
-    exports: [
-        RouterModule
-    ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginPageComponent],
+  declarations: [AppComponent, LoginPageComponent, AuthLayoutComponent, SiteLayoutComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],

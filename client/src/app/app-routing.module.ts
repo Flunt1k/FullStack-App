3858bbf89d88897e812a65200c2dfe2967fa11cd
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-04 23:05:36 +0300

    Add register page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes, Router } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AuthLayoutComponent,
-    children: [{ path: 'login', component: LoginPageComponent }],
+    children: [{ path: 'login', component: LoginPageComponent }, 
+               {path: 'register', component: RegisterPageComponent}],
   },
   {
     path: '',

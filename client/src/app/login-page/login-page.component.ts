fbd88e87a57551532d14eaa90e3125896d878c21
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-05 20:34:52 +0300

    Connect client part and server for login page

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,23 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css'],
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
   form : FormGroup
-  constructor() {}
+  aSub: Subscription
+  constructor(private auth: AuthService) {}
 
   ngOnInit(): void {
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
-      password : new FormControl(null, [Validators.required, Validators.minLength(6)])
+      password : new FormControl(null, [Validators.required, Validators.minLength(3)])
     })
   }
 
+  ngOnDestroy(){
+    if (this.aSub) this.aSub.unsubscribe()
+  }
+
   onSubmit(){
-    
+    this.form.disable()
+    this.aSub = this.auth.login(this.form.value).subscribe(
+      () => console.log('login successes'),
+      error => {
+        console.warn(error)
+        this.form.enable()
+      }
+      
+    )
   }
 }

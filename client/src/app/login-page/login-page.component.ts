5b84dd98d402cf92409a30d99d583510601a8126
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-05 20:42:03 +0300

    Add redirect after login and message for ready to login person

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../shared/services/auth.service';
 import { Subscription } from 'rxjs';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -11,14 +12,25 @@ import { Subscription } from 'rxjs';
 export class LoginPageComponent implements OnInit, OnDestroy {
   form : FormGroup
   aSub: Subscription
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password : new FormControl(null, [Validators.required, Validators.minLength(3)])
     })
+
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['registered']){
+        //Now you can login use your data
+      } else if (params['access denied']) {
+        //You need to authorize
+      }
+    })
   }
+
 
   ngOnDestroy(){
     if (this.aSub) this.aSub.unsubscribe()
@@ -27,7 +39,7 @@ export class LoginPageComponent implements OnInit, OnDestroy {
   onSubmit(){
     this.form.disable()
     this.aSub = this.auth.login(this.form.value).subscribe(
-      () => console.log('login successes'),
+      () => this.router.navigate(['/overview']),
       error => {
         console.warn(error)
         this.form.enable()

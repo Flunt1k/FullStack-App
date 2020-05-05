178c49f9f0db7a29f9040861c3ad998c2ef2b3fb
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-05 20:47:47 +0300

    Add methods for get, set, delete token and add token to localStorage

--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators'
 import { User } from '../Interfaces';
 
 @Injectable({
@@ -8,12 +9,39 @@ import { User } from '../Interfaces';
 })
 export class AuthService {
 
+    private token = null
+
     constructor(private http: HttpClient){
 
     }
 
     login(user : User) : Observable<{token: string}> {
        return this.http.post<{token: string}>('/api/auth/login', user)
+       .pipe(
+           tap(
+               ({token}) => {
+                localStorage.setItem('auth-token', token)
+                this.setToken(token)
+               }
+           )
+       )
+    }
+
+    setToken(token: string){
+        this.token = token
+    }
+
+    getToken(): string {
+        return this.token
+    }
+
+    isAuthenticated(): boolean{
+        return Boolean(this.token)
+    }
+
+    logout() {
+        this.setToken(null)
+        localStorage.clear
     }
 
     register() {}

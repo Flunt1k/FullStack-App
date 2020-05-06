1894035cc547fdbe2a165678dea980ded872c9bf
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-06 23:44:00 +0300

    Add interceptor for holding token

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -9,6 +9,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import { TokenInterceptor } from './shared/classes/token.interceptor';
 
 
 @NgModule({
@@ -19,6 +20,7 @@ import { RegisterPageComponent } from './register-page/register-page.component';
     SiteLayoutComponent,
     RegisterPageComponent,
   ],
+  providers: [{provide: HTTP_INTERCEPTORS, multi: true, useClass: TokenInterceptor}],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   bootstrap: [AppComponent],
 })

09d6e3c85069314a7206b795519405b160b06de3
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-06 23:50:11 +0300

    Add token from LocalStorage to auth session

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor (private auth: AuthService) { }
+
+  ngOnInit() {
+    const ourToken = localStorage.getItem('auth-token')
+    if (ourToken !== null) this.auth.setToken(ourToken) 
+  }
   title = 'client';
 }

b341ad968e43a90b62c89cff27fca699c7bb2dc8
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-07 21:29:19 +0300

    refactor: replace html links on loop of link's array

--- client/src/app/shared/layouts/site-layout/site-layout.component.ts
+++ client/src/app/shared/layouts/site-layout/site-layout.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-site-layout',
   templateUrl: './site-layout.component.html',
-  styleUrls: ['./site-layout.component.css']
+  styleUrls: ['./site-layout.component.css'],
 })
 export class SiteLayoutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  links = [
+    {url: '/overview', name: 'Обзор'},
+    {url: '/analytics', name: 'Аналитика'},
+    {url: '/history', name: 'История'},
+    {url: '/order', name: 'Добавление заказа'},
+    {url: '/categories', name: 'Ассортимент'},
+  ]
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

60ca2922ea84dd49882519cfbc2cfa09e49dd80f
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-08 23:22:33 +0300

    refactor: delete flag loading and subscription on stream, add async pipe

--- client/src/app/categories-page/categories-page.component.ts
+++ client/src/app/categories-page/categories-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoriesService } from '../shared/services/categories.service';
 import { Category } from '../shared/Interfaces';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-categories-page',
@@ -10,14 +11,10 @@ import { Category } from '../shared/Interfaces';
 export class CategoriesPageComponent implements OnInit {
   constructor(private categoriesService: CategoriesService) {}
 
-  loading = false
-  categories: Category[] = []
+  
+  categories$: Observable<Category[]>
 
   ngOnInit(): void {
-    this.loading = true
-    this.categoriesService.fetch().subscribe(categories => {
-      this.loading = false
-      this.categories = categories
-    })
+    this.categories$ = this.categoriesService.fetch()
   }
 }

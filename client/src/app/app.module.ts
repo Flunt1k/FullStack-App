0de76b00791ca0dbc48ddbf6add1d5fec50dfc9f
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-08 23:10:42 +0300

    feat: add loader and show loading categories from server

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AnalyticsPageComponent } from './analytics-page/analytics-page.componen
 import { HistoryPageComponent } from './history-page/history-page.component';
 import { OrderPageComponent } from './order-page/order-page.component';
 import { CategoriesPageComponent } from './categories-page/categories-page.component';
+import { LoaderComponent } from './shared/components/loader/loader.component';
 
 
 @NgModule({
@@ -29,6 +30,7 @@ import { CategoriesPageComponent } from './categories-page/categories-page.compo
     HistoryPageComponent,
     OrderPageComponent,
     CategoriesPageComponent,
+    LoaderComponent,
   ],
   providers: [{provide: HTTP_INTERCEPTORS, multi: true, useClass: TokenInterceptor}],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule],
--- client/src/app/categories-page/categories-page.component.ts
+++ client/src/app/categories-page/categories-page.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from '../shared/services/categories.service';
+import { Category } from '../shared/Interfaces';
 
 @Component({
   selector: 'app-categories-page',
   templateUrl: './categories-page.component.html',
-  styleUrls: ['./categories-page.component.css']
+  styleUrls: ['./categories-page.component.css'],
 })
 export class CategoriesPageComponent implements OnInit {
+  constructor(private categoriesService: CategoriesService) {}
 
-  constructor() { }
+  loading = false
+  categories: Category[] = []
 
   ngOnInit(): void {
+    this.loading = true
+    this.categoriesService.fetch().subscribe(categories => {
+      this.loading = false
+      this.categories = categories
+    })
   }
-
 }

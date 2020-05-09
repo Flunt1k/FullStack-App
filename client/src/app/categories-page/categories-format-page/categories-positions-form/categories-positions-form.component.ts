d7c599e2bf08bd65f14b9cde386d7e41cedc466c
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-09 23:58:14 +0300

    feat: add modal window for adding positions

--- client/src/app/categories-page/categories-format-page/categories-positions-form/categories-positions-form.component.ts
+++ client/src/app/categories-page/categories-format-page/categories-positions-form/categories-positions-form.component.ts
@@ -1,26 +1,58 @@
-import { Component, OnInit, Input } from "@angular/core"
-import { PositionService } from 'src/app/shared/services/position.service'
-import { Position } from 'src/app/shared/Interfaces'
+import {
+	Component,
+	OnInit,
+	Input,
+	ViewChild,
+	ElementRef,
+	AfterViewInit,
+  OnDestroy,
+} from "@angular/core"
+import { PositionService } from "src/app/shared/services/position.service"
+import { Position } from "src/app/shared/Interfaces"
+import {
+	MaterialService,
+	MaterialInterface,
+} from "src/app/shared/classes/material.service"
 
 @Component({
 	selector: "app-categories-positions-form",
 	templateUrl: "./categories-positions-form.component.html",
 	styleUrls: ["./categories-positions-form.component.css"],
 })
-export class CategoriesPositionsFormComponent implements OnInit {
-  @Input('categoryId') categoryId: string
-  constructor(private positionService: PositionService) {}
-  
-  positions: Position[] = []
-  loading = false
+export class CategoriesPositionsFormComponent implements OnInit, AfterViewInit, OnDestroy {
+	@Input("categoryId") categoryId: string
+	@ViewChild("modal") modalRef: ElementRef
+	constructor(private positionService: PositionService) {}
+
+	positions: Position[] = []
+	loading = false
+	modal: MaterialInterface
 
 	ngOnInit(): void {
-    this.loading = true
-    this.positionService.fetch(this.categoryId).subscribe(
-      positions => {
-        this.positions = positions
-        this.loading = false
-      }
-    )
+		this.loading = true
+		this.positionService.fetch(this.categoryId).subscribe((positions) => {
+			this.positions = positions
+			this.loading = false
+		})
+	}
+
+	ngAfterViewInit() {
+		this.modal = MaterialService.initModal(this.modalRef)
+  }
+  
+  ngOnDestroy() {
+    this.modal.destroy()
+  }
+
+  onSelectPosition(position: Position) {
+    this.modal.open()
+  }
+
+  onAddPosition() {
+    this.modal.open()
+  }
+
+  onClose() {
+    this.modal.close()
   }
 }

edb5f9a33c5c6030af1105e87b3696a8d757384c
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-11 19:16:23 +0300

    feat: add modal window

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,21 +1,54 @@
-import { Component, OnInit } from "@angular/core"
-import { Router, NavigationEnd } from '@angular/router'
+import {
+	Component,
+	OnInit,
+	ViewChild,
+	ElementRef,
+	OnDestroy,
+	AfterViewInit,
+} from "@angular/core"
+import { Router, NavigationEnd } from "@angular/router"
+import {
+	MaterialService,
+	MaterialInterface,
+} from "../shared/classes/material.service"
 
 @Component({
 	selector: "app-order-page",
 	templateUrl: "./order-page.component.html",
 	styleUrls: ["./order-page.component.css"],
 })
-export class OrderPageComponent implements OnInit {
-  constructor(private router: Router) {}
-  
-  isRoot: boolean
+export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
+	constructor(private router: Router) {}
+
+	@ViewChild("modal") modalRef: ElementRef
+	isRoot: boolean
+	modal: MaterialInterface
 
 	ngOnInit(): void {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd){
-        this.isRoot = this.router.url === '/order'
-      }
-    })
-  }
+		this.router.events.subscribe((event) => {
+			if (event instanceof NavigationEnd) {
+				this.isRoot = this.router.url === "/order"
+			}
+		})
+	}
+
+	ngOnDestroy() {
+		this.modal.destroy()
+	}
+
+	ngAfterViewInit() {
+	  this.modal =	MaterialService.initModal(this.modalRef)
+	}
+
+	openWindow() {
+		this.modal.open()
+	}
+
+	closeWindow() {
+		this.modal.close()
+	}
+
+	onSubmit() {
+		this.modal.close()
+	}
 }

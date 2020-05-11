1fe50119e926625682bb14af17266fc3e37921e7
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-11 21:30:48 +0300

    refactor: show date, price, cost and activate modal window

--- client/src/app/history-page/history-list/history-list.component.ts
+++ client/src/app/history-page/history-list/history-list.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit, Input } from "@angular/core"
-import { Order } from 'src/app/shared/Interfaces'
+import {
+	Component,
+	Input,
+	ViewChild,
+	ElementRef,
+	OnDestroy,
+	AfterViewInit,
+} from "@angular/core"
+import { Order } from "src/app/shared/Interfaces"
+import {
+	MaterialService,
+	MaterialInterface,
+} from "src/app/shared/classes/material.service"
 
 @Component({
 	selector: "app-history-list",
 	templateUrl: "./history-list.component.html",
 	styleUrls: ["./history-list.component.css"],
 })
-export class HistoryListComponent implements OnInit {
-	constructor() {}
-
+export class HistoryListComponent implements OnDestroy, AfterViewInit {
 	@Input() orders: Order[]
+	@ViewChild("modal") modalRef: ElementRef
+	modal: MaterialInterface
+	selectedOrder: Order
 
-	ngOnInit(): void {}
+	ngOnDestroy() {
+		this.modal.destroy()
+	}
+
+	ngAfterViewInit() {
+		this.modal = MaterialService.initModal(this.modalRef)
+	}
+
+	calcPrice(order: Order): number {
+		return order.list.reduce((acc, item) => {
+			return (acc += item.quantity * item.cost)
+		}, 0)
+	}
+
+	showModal(order: Order) {
+		this.selectedOrder = order
+		this.modal.open()
+	}
+
+	closeWindow() {
+		this.modal.close()
+	}
 }

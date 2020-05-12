e11bca4f42c5dff1e413f617003a4a3388203fec
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-12 21:58:22 +0300

    feat: implement analytics controller to backend

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -4,7 +4,7 @@ const errorHandler = require("../utils/errorHandler")
 
 module.exports.overview = async function (request, response) {
 	try {
-		const orders = await Order.find({ user: request.user.id }).sort({date: 1})
+		const orders = await Order.find({ user: request.user.id }).sort({ date: 1 })
 		const ordersMap = getOrdersMap(orders)
 		const ordersTheDayBefore =
 			ordersMap[momentJS().add(-1, "d").format("DD.MM.YYYY")] || []
@@ -49,36 +49,62 @@ module.exports.overview = async function (request, response) {
 		const revenuePercent = (
 			(revenueTheDayBefore / revenuePerDay - 1) *
 			100
-        ).toFixed(2)
-        
-        //Compare revenue
+		).toFixed(2)
 
-        const compareRevenue = (revenueTheDayBefore - revenuePerDay).toFixed(2)
+		//Compare revenue
 
-        //Compare number of orders
+		const compareRevenue = (revenueTheDayBefore - revenuePerDay).toFixed(2)
 
-        const compareNumber = (ordersTheDayBeforeNumber - ordersPerDay).toFixed(2)
+		//Compare number of orders
 
-        response.status(200).json({
-            revenue: {
-                percent: Math.abs(+revenuePercent),
-                compare: Math.abs(+compareRevenue),
-                yesterday: +revenueTheDayBefore,
-                isHigher: +revenuePercent > 0
-            },
-            orders: {
-                percent: Math.abs(+ordersPercent),
-                compare: Math.abs(+compareNumber),
-                yesterday: +ordersTheDayBefore,
-                isHigher: +ordersPercent > 0
-            }
-        })
+		const compareNumber = (ordersTheDayBeforeNumber - ordersPerDay).toFixed(2)
+
+		response.status(200).json({
+			revenue: {
+				percent: Math.abs(+revenuePercent),
+				compare: Math.abs(+compareRevenue),
+				yesterday: +revenueTheDayBefore,
+				isHigher: +revenuePercent > 0,
+			},
+			orders: {
+				percent: Math.abs(+ordersPercent),
+				compare: Math.abs(+compareNumber),
+				yesterday: +ordersTheDayBefore,
+				isHigher: +ordersPercent > 0,
+			},
+		})
 	} catch (error) {
 		errorHandler(response, error)
 	}
 }
 
-module.exports.analytics = function (request, response) {}
+module.exports.analytics = async function (request, response) {
+	try {
+		const orders = await Order.find({ user: request.user.id }).sort({ date: 1 })
+		const ordersMap = getOrdersMap(orders)
+
+		const average = +(
+			calcRevenue(orders) / Object.keys(ordersMap).length
+		).toFixed(2)
+
+		const chart = Object.keys(ordersMap).map((label) => {
+			const revenue = calcRevenue(ordersMap[label])
+			const order = ordersMap[label].length
+			return {
+				label,
+				order,
+				revenue,
+			}
+		})
+
+		response.status(200).json({
+			average,
+			chart,
+		})
+	} catch (error) {
+		errorHandler(response, error)
+	}
+}
 
 function getOrdersMap(orders = []) {
 	const dayAndOrders = {}
